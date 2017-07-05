90446127b72ebd8a7115f5a84595336db12978f1
lorentzenj <jess.lorentzen@meredith.com>
2017-07-05 10:19:30 -0500

    Check servers for minidump files
    Checks a list of servers for a minidump file or *.dmp.  Servers that have Bluescreened and crashed will have these memory dumps.

---
+++ CheckDCDump.ps1
@@ -0,0 +1,18 @@
+﻿#checks DC list for dump files.
+
+$outfile = "C:\scripts\minidump.txt"
+
+$servers = get-content C:\scripts\DCs.txt
+
+foreach ($server in $servers){
+
+$fileExists = Test-Path \\$server\C$\Windows\Minidump\*.dmp
+
+if ($fileExists){
+    
+    $server | Out-File $outfile -Append
+    Get-ChildItem \\$server\C$\Windows\Minidump | Out-File $outfile -Append
+
+
+    }
+}
